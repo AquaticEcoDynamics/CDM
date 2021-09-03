344fa2d1a21ec158edfd9f5f4aa489440666c172
brendan-busch <brendan.busch@uwa.edu.au>
2021-09-03 08:17:35 +0800

    Update create_large_files.m

--- scripts/create_large_files.m
+++ scripts/create_large_files.m
@@ -1,3 +1,13 @@
 % Script to recreate the larger files for the CDM repo.
 
-run('datatools\dataprocessing\join_datasources\merge_all_data.m');
+% Join smaller mat files into the cllmm.mat file & cllmm_bc.mat file (both
+% >100mb).
+% run('datatools\dataprocessing\join_datasources\merge_all_data.m');
+
+
+% Download & import the Water Data SA hourly data to your local machine.
+
+% run('dataimport\hydro\WaterDataSA\download_datafiles.m');
+% run('dataimport\hydro\WaterDataSA\download_datafiles.m');
+
+
