34122d96825302e3ecfbf163b330d5d1ba8d12df
brendan-busch <brendan.busch@uwa.edu.au>
2021-09-10 19:41:16 +0800

    removing older scripts: can be found in dropbox

--- scripts/modeltools/olderscripts/cew/PTM/convert_grids.m
+++
@@ -1,9 +0,0 @@
-clear all; close all;
-
-addpath(genpath('functions'));
-
-dirlist = dir(['MatGrids/','*.2dm']);
-
-for i = 1:length(dirlist)
-    convert_2dm_to_shp(['MatGrids/',dirlist(i).name]);
-end
