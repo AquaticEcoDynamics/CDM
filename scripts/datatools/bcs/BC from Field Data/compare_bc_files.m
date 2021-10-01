2cebeefc2f61394ec1d5ba846a4d40ee20f7def1
brendan-busch <brendan.busch@uwa.edu.au>
2021-10-01 13:31:57 +0800

    Phase 2 CIIP BC files

--- scripts/datatools/bcs/BC from Field Data/compare_bc_files.m
+++ scripts/datatools/bcs/BC from Field Data/compare_bc_files.m
@@ -4,6 +4,7 @@ alex = load('BCs_BAR_2012_2021_Monthly_Ave_New/Alex_Mid/data.mat');
 albert = load('BCs_BAR_2012_2021_Monthly_Ave_New/Albert_Opening/data.mat'); 
 Meningie = load('BCs_BAR_2012_2021_Monthly_Ave_New/Albert_Meningie/data.mat'); 
 Milang = load('BCs_BAR_2012_2021_Monthly_Ave_New/Milang/data.mat'); 
+Tailem = load('BCs_BAR_2012_2021_Monthly_Ave_New/Tailem/data.mat'); 
 
 
 vars = fieldnames(alex.tfv_data);
@@ -13,12 +14,13 @@ mkdir('BCs_BAR_2012_2021_Monthly_Ave_New/Alex_Albert_Compare/');
 for i = 1:length(vars)
     
 %     plot(alex.ISOTime,alex.tfv_data.(vars{i}));hold on
-    plot(albert.ISOTime,albert.tfv_data.(vars{i}));hold on
     plot(Meningie.ISOTime,Meningie.tfv_data.(vars{i}));hold on
     plot(Milang.ISOTime,Milang.tfv_data.(vars{i}));hold on
+    plot(Tailem.ISOTime,Tailem.tfv_data.(vars{i}));hold on
+
     datetick('x');
     
-    legend({'Albert Open';'Meningie';'Milang'});
+    legend({'Meningie';'Milang';'Tailem Bend'});
     
     ylabel(regexprep(vars{i},'_',' '));
     xlabel('Date');
---
+++ scripts/datatools/bcs/compare_bcfiles/compare_albert_simulation_bcs.m
@@ -0,0 +1,75 @@
+clear all; close all;
+
+addpath(genpath('../../../../../aed_matlab_modeltools/TUFLOWFV/tuflowfv/'));
+
+outdir = 'Images/COMP/'; mkdir(outdir);
+
+albert_old = tfv_readBCfile(['Phase2\',...
+    'LAC_BC_20140101_20200701_800GLy-1_realistic.csv']);
+
+albert_new = tfv_readBCfile(['Phase2\',...
+    'Albert_Phase_II_20140101_20200701.csv']);
+
+alex_new = tfv_readBCfile(['Phase2\',...
+    'Alex_Phase_II_20140101_20200701.csv']);
+
+river_new = tfv_readBCfile(['Phase2\',...
+    'River_Phase_II_20120101_20210701.csv']);
+
+vars = fieldnames(river_new);
+
+for i = 1:length(vars)
+    
+    if  strcmpi(vars{i},'Date') == 0 
+        
+        
+        fig1 = figure('visible','off');
+        set(fig1,'defaultTextInterpreter','latex')
+        set(0,'DefaultAxesFontName','Times')
+        set(0,'DefaultAxesFontSize',6)
+        
+        plot(albert_old.Date,albert_old.(vars{i}));hold on
+        
+        plot(albert_new.Date,albert_new.(vars{i}));hold on
+        
+        plot(alex_new.Date,alex_new.(vars{i}));hold on
+        
+        plot(river_new.Date,river_new.(vars{i}));hold on
+        
+        
+        
+        datearray = datenum(2015:01:2021,01,01);
+        
+        
+        xlim([datearray(1) datearray(end)]);
+        
+        ylabel(regexprep(vars{i},'_',' '));
+        xlabel('Date');
+                
+        grid on
+        
+        set(gca,'xtick',datearray,'xticklabel',datestr(datearray,'yyyy'));
+        
+        legend({'Albert ph.1';'Albert ph.2';'Alex ph.2';'River ph.2'});
+        
+        set(gcf, 'PaperPositionMode', 'manual');
+        set(gcf, 'PaperUnits', 'centimeters');
+        xSize = 16;
+        ySize = 8;
+        xLeft = (21-xSize)/2;
+        yTop = (30-ySize)/2;
+        set(gcf,'paperposition',[0 0 xSize ySize])
+        
+        filename = [outdir,vars{i},'.png'];
+        
+        
+        print(gcf,'-dpng',filename,'-opengl');
+        
+        close
+        
+    end
+    
+end
+        
+ create_html_for_directory('Images/');
+        
