0a667f81629f2ecdbb85471d5e47dec10b72a9b5
brendan-busch <brendan.busch@uwa.edu.au>
2021-11-15 15:57:40 +0800

    Update export_model_data_for_sed_diag.m

--- scripts/modeltools/SDG_export/export_model_data_for_sed_diag.m
+++ scripts/modeltools/SDG_export/export_model_data_for_sed_diag.m
@@ -1,15 +1,15 @@
-addpath(genpath('../../../../aed_matlab_modeltools/TUFLOWFV/tuflowfv/'));
+addpath(genpath('functions'));
 
-ncfile = 'W:\CIIP\CIIP_Phase2_sensitivity_assessment_v1\output_SC01_S2\CoorongBGC_SC01_base_dry_001_S2_all.nc';
+ncfile = 'Z:\Busch\Studysites\Peel\2018_Modelling\Peel_WQ_Model_v4_benthic\Output\benthic_08flow\sim_2016_Open_2D.nc';
 
 shp = shaperead('Export_Locations.shp');
 
-%sites = {'LM3G';'WP1G'}; % mat zones 18 & 9
+sites = {'LM3G';'WP1G'}; % mat zones 18 & 9
 
 
 
 data = tfv_readnetcdf(ncfile,'timestep',1);
-mdata = tfv_readnetcdf(ncfile,'names',{'WQ_OXY_OXY';'WQ_NIT_NIT';'WQ_NIT_AMM';'WQ_PHS_FRP';'WQ_DIAG_OGM_PSED_POC'});
+mdata = tfv_readnetcdf(ncfile,'names',{'WQ_OXY_OXY';'WQ_NIT_NIT';'WQ_NIT_AMM';'WQ_OGM_POC';'WQ_PHS_FRP'});
 dat = tfv_readnetcdf(ncfile,'time',1);
 tdate = dat.Time;
 
@@ -17,13 +17,16 @@ cX = double(data.cell_X);
 cY = double(data.cell_Y);
 dtri = DelaunayTri(cX,cY);
 
-for i = 1:length(shp)
+for i = 1:length(sites)
     
+    for j = 1:length(shp)
+        if strcmpi(shp(j).Name,sites{i}) == 1
+            ind = j;
+        end
+    end
     
-    X = shp(i).X;
-    Y = shp(i).Y;
-    
-    sites = shp(i).Code;
+    X = shp(ind).X;
+    Y = shp(ind).Y;
     
    pt_id = nearestNeighbor(dtri,[X Y]);
    
@@ -31,28 +34,25 @@ for i = 1:length(shp)
    oxy = mdata.WQ_OXY_OXY(pt_id,:);
    nit = mdata.WQ_NIT_NIT(pt_id,:);
    amm = mdata.WQ_NIT_AMM(pt_id,:);
+   poc = mdata.WQ_OGM_POC(pt_id,:);
    frp = mdata.WQ_PHS_FRP(pt_id,:); 
-   poc = mdata.WQ_DIAG_OGM_PSED_POC(pt_id,:);
-
-   poc = abs(poc);
    
-   poc = poc * (86400*365);
    
    tdate = tdate - tdate(1);
    
    
-   fid = fopen(['aed2_sediment_swibc_',sites,'.dat'],'wt');
+   fid = fopen(['aed2_sediment_swibc_',sites{i},'.dat'],'wt');
    
    
-   fprintf(fid,'!				1				2			3			4			5			6			7,\n');
+   fprintf(fid,'!				1				2			3			4			5			6			7\n');
    fprintf(fid,'&bc\n');
-   fprintf(fid,'headings1 = 	timestep, 	time, 	      oxy,		  nit,		  amm,		 frp, 	      pocl,\n');
-   fprintf(fid,'forzone   =  	0,			   0,			1,			1,			1,			1,			1,\n');
-   fprintf(fid,'swibc 	  =     1,             0,       %4.4f,      %4.4f,      %4.4f,      %4.4f,      %4.4f,\n',...
-       oxy(1),nit(1),amm(1),frp(1),poc(1));
+   fprintf(fid,'headings1 = 	timestep, 	time, 	      oxy,		  nit,		  amm,		 poml, 	      frp\n');
+   fprintf(fid,'forzone   =  	0,			   0,			1,			1,			1,			1,			1\n');
+   fprintf(fid,'swibc 	  =     1,             0,       %4.4f,      %4.4f,      %4.4f,      %4.4f,      %4.4f\n',...
+       oxy(1),nit(1),amm(1),poc(1),frp(1));
    for j = 2:length(oxy)
-       fprintf(fid,'                %d,         %4.2f,       %4.4f,      %4.4f,      %4.4f,      %4.4f,      %4.4f,\n',...
-           j,tdate(j),oxy(j),nit(j),amm(j),frp(j),poc(j));
+       fprintf(fid,'                %d,         %4.2f       %4.4f,      %4.4f,      %4.4f,      %4.4f,      %4.4f\n',...
+           j,tdate(j),oxy(j),nit(j),amm(j),poc(j),frp(j));
    end
    fclose(fid);
 end
