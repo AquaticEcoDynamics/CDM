dc999fbb34720ca45801d2958f6e0d01bf07b708
brendan-busch <brendan.busch@uwa.edu.au>
2021-12-21 10:23:21 +0800

    Update to roughness script

--- scripts/datatools/zonetransfer/roughness/create_roughness_material.m
+++ scripts/datatools/zonetransfer/roughness/create_roughness_material.m
@@ -4,10 +4,20 @@ fid = fopen('Material_Roughness_z31.fvc','wt');
 
 shp = shaperead('../GIS/31_material_zones.shp');
 
+[~,zones] = xlsread('Roughness_Values.xlsx','B2:B100');
+
+for i = 1:length(zones)
+    zoneID(i) = str2num(regexprep(zones{i},'Zone ',''));
+end
+
+[vals,~] = xlsread('Roughness_Values.xlsx','C2:C100');
+
 for i = 1:length(shp)
     
+    sss = find(zoneID == shp(i).Mat);
+    
     fprintf(fid,'material == %d\n',shp(i).Mat);
-    fprintf(fid,'bottom roughness == 0.018\n');
+    fprintf(fid,'bottom roughness == %4.4f\n',vals(sss));
     fprintf(fid,'end material\n');
     fprintf(fid,'\n');
     fprintf(fid,'\n');
