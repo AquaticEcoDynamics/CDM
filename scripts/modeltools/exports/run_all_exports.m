aba5a09278c955152d2938205f930215c64c342e
brendan-busch <brendan.busch@uwa.edu.au>
2022-06-30 09:13:20 +0800

    Update run_all_exports.m

--- scripts/modeltools/exports/run_all_exports.m
+++ scripts/modeltools/exports/run_all_exports.m
@@ -4,87 +4,87 @@ filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/outp
 outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_basecase_hd_v2/Exports/';
 
 
+draft_export_2(filename,outputdir);
+draft_export_3(filename,outputdir);
+draft_export_4(filename,outputdir);
+draft_export_5(filename,outputdir);
+draft_export_6(filename,outputdir);
+draft_export_7(filename,outputdir);
+draft_export_8(filename,outputdir);
+draft_export_9(filename,outputdir);
+draft_export_10(filename,outputdir);
+draft_export_11(filename,outputdir);
+draft_export_12(filename,outputdir);
+draft_export_13(filename,outputdir);
+draft_export_14(filename,outputdir);
+
+%filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_noeWater_v2/hchb_Gen2_201707_202201_noeWater_all.nc';
+%outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_noeWater_v2/Exports/';
+%
+%
 %draft_export_2(filename,outputdir);
 %draft_export_3(filename,outputdir);
 %draft_export_4(filename,outputdir);
 %draft_export_5(filename,outputdir);
 %draft_export_6(filename,outputdir);
 %draft_export_7(filename,outputdir);
-draft_export_8(filename,outputdir);
+%draft_export_8(filename,outputdir);
 %draft_export_9(filename,outputdir);
 %draft_export_10(filename,outputdir);
 %draft_export_11(filename,outputdir);
 %draft_export_12(filename,outputdir);
 %draft_export_13(filename,outputdir);
 %draft_export_14(filename,outputdir);
-
-filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_noeWater_v2/hchb_Gen2_201707_202201_noeWater_all.nc';
-outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_noeWater_v2/Exports/';
-
-
+%
+%filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_noSE_v2/hchb_Gen2_201707_202201_noSE_all.nc';
+%outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_noSE_v2/Exports/';
+%
+%
 %draft_export_2(filename,outputdir);
 %draft_export_3(filename,outputdir);
 %draft_export_4(filename,outputdir);
 %draft_export_5(filename,outputdir);
 %draft_export_6(filename,outputdir);
 %draft_export_7(filename,outputdir);
-draft_export_8(filename,outputdir);
+%draft_export_8(filename,outputdir);
 %draft_export_9(filename,outputdir);
 %draft_export_10(filename,outputdir);
 %draft_export_11(filename,outputdir);
 %draft_export_12(filename,outputdir);
 %draft_export_13(filename,outputdir);
 %draft_export_14(filename,outputdir);
-
-filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_noSE_v2/hchb_Gen2_201707_202201_noSE_all.nc';
-outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_noSE_v2/Exports/';
-
-
+%
+%
+%filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_scen3a_v2/hchb_Gen2_201707_202201_scen3a_all.nc';
+%outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_scen3a_v2/Exports/';
+%
+%
 %draft_export_2(filename,outputdir);
 %draft_export_3(filename,outputdir);
 %draft_export_4(filename,outputdir);
 %draft_export_5(filename,outputdir);
 %draft_export_6(filename,outputdir);
 %draft_export_7(filename,outputdir);
-draft_export_8(filename,outputdir);
+%draft_export_8(filename,outputdir);
 %draft_export_9(filename,outputdir);
 %draft_export_10(filename,outputdir);
 %draft_export_11(filename,outputdir);
 %draft_export_12(filename,outputdir);
 %draft_export_13(filename,outputdir);
 %draft_export_14(filename,outputdir);
-
-
-filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_scen3a_v2/hchb_Gen2_201707_202201_scen3a_all.nc';
-outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_scen3a_v2/Exports/';
-
-
+%
+%
+%filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_scen3b_v2/hchb_Gen2_201707_202201_scen3b_all.nc';
+%outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_scen3b_v2/Exports/';
+%
+%
 %draft_export_2(filename,outputdir);
 %draft_export_3(filename,outputdir);
 %draft_export_4(filename,outputdir);
 %draft_export_5(filename,outputdir);
 %draft_export_6(filename,outputdir);
 %draft_export_7(filename,outputdir);
-draft_export_8(filename,outputdir);
-%draft_export_9(filename,outputdir);
-%draft_export_10(filename,outputdir);
-%draft_export_11(filename,outputdir);
-%draft_export_12(filename,outputdir);
-%draft_export_13(filename,outputdir);
-%draft_export_14(filename,outputdir);
-
-
-filename = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_scen3b_v2/hchb_Gen2_201707_202201_scen3b_all.nc';
-outputdir = '/Projects2/CDM/HCHB_scenario_assessment/HCHB_GEN2_4yrs_20220620/output_scen3b_v2/Exports/';
-
-
-%draft_export_2(filename,outputdir);
-%draft_export_3(filename,outputdir);
-%draft_export_4(filename,outputdir);
-%draft_export_5(filename,outputdir);
-%draft_export_6(filename,outputdir);
-%draft_export_7(filename,outputdir);
-draft_export_8(filename,outputdir);
+%draft_export_8(filename,outputdir);
 %draft_export_9(filename,outputdir);
 %draft_export_10(filename,outputdir);
 %draft_export_11(filename,outputdir);
